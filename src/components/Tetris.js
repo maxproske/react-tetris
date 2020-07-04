371da21e2474ac6942b934b2d65aab60c4867bb6
Max Proske <mp@six12creative.com>
2020-07-03 22:51:50 -0700

    Add player movement

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -11,6 +11,7 @@ import { usePlayer } from '../hooks/usePlayer'
 import { useStage } from '../hooks/useStage'
 
 import bgImage from '../img/bg.png'
+import { createStage } from '../gameHelpers'
 
 const StyledTetrisWrapper = styled.div`
   width: 100vw;
@@ -39,13 +40,52 @@ export const Tetris = () => {
   const [dropTime, setDropTime] = useState(null)
   const [gameOver, setGameOver] = useState(false)
 
-  const [player] = usePlayer()
+  const [player, updatePlayerPos, resetPlayer] = usePlayer()
   const [stage, setStage] = useStage(player)
 
   console.log('re-render')
 
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 })
+  }
+
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage())
+    resetPlayer()
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop()
+  }
+
+  // Callback function for keyboard input
+  const move = ({ keyCode }) => {
+    console.log('KEY PRESS', keyCode)
+
+    if (!gameOver) {
+      // Left
+      if (keyCode === 37) {
+        movePlayer(-1)
+      }
+      // Right
+      else if (keyCode === 39) {
+        movePlayer(1)
+      }
+      // Down
+      else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
+  }
+
+  // Note: Without role="button", you would have to click the stage for inputs to register
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -58,7 +98,7 @@ export const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,10 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { createStage } from '../gameHelpers'
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   // Set initial stage
   const [stage, setStage] = useState(createStage())
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // Clear previous render
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+      )
+
+      // Draw next render
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          // Check which cells are occupied
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              player.collided ? 'merged' : 'clear',
+            ]
+          }
+        })
+      })
+
+      return newStage
+    }
+
+    // Will infinite loop unless we invoke useCallback
+    setStage((prev) => updateStage(prev))
+  }, [player])
 
   return [stage, setStage]
 }
